af483121aa9ac8bb4fa5b1f431e505beab10fd30
kopytovs <kopytov@me.com>
2017-02-22 23:06:54 +0300

    1.1

--- Pods/Target Support Files/Nuke-Alamofire-Plugin/Nuke-Alamofire-Plugin-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double NukeAlamofirePluginVersionNumber;
-FOUNDATION_EXPORT const unsigned char NukeAlamofirePluginVersionString[];
-
