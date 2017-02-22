f63b81f82426abd066776e224af15c94d7d3772c
kopytovs <kopytov@me.com>
2017-02-22 20:41:35 +0300

    0.1

---
+++ Test_eKassir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_eKassir
+//
+//  Created by Sergey Kopytov on 22.02.17.
+//  Copyright © 2017 Sergey Kopytov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
